4a7d7955a9e08e78b15fcac34185d115f559d0fa
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2023-05-31 01:19:16 +0700

    KASIR SELESAI

--- left-sidebar-kasir.php
+++ left-sidebar-kasir.php
@@ -33,6 +33,14 @@
                         <span class="hide-menu">Pesanan</span>
                     </a>
                 </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="riwayat-pesanan-kasir.php" aria-expanded="false">
+                        <span class="material-symbols-outlined">
+                            history
+                        </span>
+                        <span class="hide-menu">Riwayat Pesanan</span>
+                    </a>
+                </li>
                 <li class="nav-small-cap">
                     <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
                     <span class="hide-menu">UI COMPONENTS</span>
--- riwayat-pesanan-kasir.php
+++ riwayat-pesanan-kasir.php
@@ -66,115 +66,16 @@
       </header>
       <!--  Header End -->
       <div class="container-fluid">
-        <!--  Row 1 -->
-        <div class="row">
-          <h4 class="fw-bold mb-4">Pesanan</h4>
-          <!-- Sales Overview Used to be Here -->
-          <?php
-          include "conn.php";
-          $query_mysql = mysqli_query($mysqli, "SELECT * FROM progress_pesanan WHERE id_progress=1");
-          while ($data = mysqli_fetch_array($query_mysql)) { ?>
-            <div class="col-sm-4 col-lg-3">
-              <!-- Cuci -->
-              <a href="">
-                <div class="card">
-                  <div class="card-body">
-                    <div class="row alig n-items-start">
-                      <div class="col-8">
-                        <h5 class="card-title mb-9 fw-semibold">Cuci</h5>
-                        <h4 class="fw-semibold mb-3">
-                          <?php
-                          echo $data['cuci'];
-                          ?> %
-                        </h4>
-                      </div>
-                      <div class="col-4">
-                        <div class="d-flex justify-content-end">
-                          <div
-                            class="text-white bg-secondary rounded-circle p-6 d-flex align-items-center justify-content-center">
-                            <span class="material-symbols-outlined">
-                              local_laundry_service
-                            </span>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </a>
-            </div>
-            <div class="col-sm-4 col-lg-3">
-              <!-- Setrika -->
-              <a href="">
-                <div class="card">
-                  <div class="card-body">
-                    <div class="row alig n-items-start">
-                      <div class="col-8">
-                        <h5 class="card-title mb-9 fw-semibold">Setrika</h5>
-                        <h4 class="fw-semibold mb-3">
-                          <?php
-                          echo $data['setrika'];
-                          ?> %
-                        </h4>
-                      </div>
-                      <div class="col-4">
-                        <div class="d-flex justify-content-end">
-                          <div
-                            class="text-white bg-secondary rounded-circle p-6 d-flex align-items-center justify-content-center">
-                            <span class="material-symbols-outlined"> iron </span>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </a>
-            </div>
 
-            <div class="col-sm-4 col-lg-3">
-              <!-- Pengiriman -->
-              <a href="">
-                <div class="card">
-                  <div class="card-body">
-                    <div class="row alig n-items-start">
-                      <div class="col-8">
-                        <h5 class="card-title mb-9 fw-semibold">
-                          Pengiriman
-                        </h5>
-                        <h4 class="fw-semibold mb-3">
-                          <?php
-                          echo $data['pengiriman'];
-                          ?> %
-                        </h4>
-                      </div>
-                      <div class="col-4">
-                        <div class="d-flex justify-content-end">
-                          <div
-                            class="text-white bg-secondary rounded-circle p-6 d-flex align-items-center justify-content-center">
-                            <span class="material-symbols-outlined">
-                              motorcycle
-                            </span>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </a>
-            </div>
-          </div>
-          <?php
-          }
-          ?>
         <?php
         include "conn.php";
         $query_mysql = mysqli_query($mysqli, "SELECT * FROM pesanan");
         ?>
         <div class="row">
+          <p class="fw-bold mb-4 h3">Riwayat Pesanan</p>
           <div class="col-lg-10 d-flex align-items-stretch">
             <div class="card w-100">
               <div class="card-body p-4">
-                <h5 class="card-title fw-semibold mb-4">Pesanan Saya</h5>
                 <div class="table-responsive">
                   <table class="table text-nowrap mb-0 align-middle">
                     <thead class="text-dark fs-4">
